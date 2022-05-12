6228fe784d4bd3ef1e4b207d0f3f70f3a2b051aa
goan-coder <abhinavlalwani@gmail.com>
2022-05-13 00:08:15 +0200

    add kialo labels

--- data/experiments_round2/merge_edu_kialo.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-edu = pd.read_csv('../../data/edu_all_new.csv')[['source_article', 'new_label_2']]
-kialo = pd.read_csv('../../data/kialo_all.csv')[['claim', 'labels']]
-edu.new_label_2 = edu.new_label_2.apply(lambda x: [x])
-edu_v2 = pd.concat([edu.rename(columns={'source_article': 'claim', 'new_label_2': 'labels'}), kialo], ignore_index=True)
-edu_v2.to_csv('../../data/edu_v2_all.csv')
